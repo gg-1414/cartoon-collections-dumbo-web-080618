13ff9d81b9e174b2b26798ef91e75d8faed69cb2
Gina Lee <ginalee1114@gmail.com>
2018-07-24 18:32:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|call| call
+  calls.any? {|call| call.size > 4}
 end
 
 def find_the_cheese# code an argument here
